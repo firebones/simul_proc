2de51fa0ca15a022acba4519978571cd29ff6159
Quentin <salmeron.quentin@etu.unice.fr>
2014-06-10 09:37:04 +0200

    Déplacement de la doc dans le header

--- exec.c
+++ exec.c
@@ -4,150 +4,66 @@
 #include "exec.h"
 
 
-//! Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool illop_fonc(Machine pmach, Instruction instr)
 {
 	error(ERR_ILLEGAL, pmach->_pc - 1);
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool nop_fonc(Machine pmach, Instruction instr)
 {
 	return true;
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool load_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool store_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool add_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool sub_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool branch_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool call_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool ret_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool push_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool pop_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-//!	Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return
- */
 static bool halt_fonc(Machine pmach, Instruction instr)
 {
 	
 }
 
-/*!
- * \file exec.c
- * \brief Implémentation de exec.h. Exécute une instruction.
- */
-
-//! Décodage et exécution d'une instruction
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return faux après l'exécution de \c HALT ; vrai sinon
- */
 bool decode_execute(Machine *pmach, Instruction instr)
 {
 	switch(instr.instr_generic._cop)
@@ -181,15 +97,6 @@ bool decode_execute(Machine *pmach, Instruction instr)
 	}
 }
 
-//! Trace de l'exécution
-/*!
- * On écrit l'adresse et l'instruction sous forme lisible.
- *
- * \param msg le message de trace
- * \param pmach la machine en cours d'exécution
- * \param instr l'instruction à exécuter
- * \param addr son adresse
- */
 void trace(const char *msg, Machine *pmach, Instruction instr, unsigned addr)
 {
 	printf("TRACE : %s: 0x%04x: ", msg, addr);
