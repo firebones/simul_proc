1fc8abc88ca289379ef1635ca89e77e15b10cb47
Tianhao shi <shi.tianhao@etu.unice.fr>
2014-06-11 13:13:04 +0200

    change printf to strerr in error.c

--- error.c
+++ error.c
@@ -12,30 +12,32 @@ void warning(Warning warn, unsigned addr)
 
 void error(Error err, unsigned addr)
 {
-	printf("Error: ");
+	fprintf(stderr,"Error: ");
 	switch(err){
 		case ERR_UNKNOWN: 
-			printf("Unknowed instruction at address 0x%x\n",addr); 
+			fprintf(stderr,"Unknowed instruction at address 0x%x\n",addr); 
 			exit(1);
     	case ERR_ILLEGAL:
-    		printf("Illegal instruction at address 0x%x\n",addr);	
+    		fprintf(stderr,"Illegal instruction at address 0x%x\n",addr);	
     		exit(1);
     	case ERR_CONDITION:
-    		printf("Illegal condition at address 0x%x\n",addr);
+    		fprintf(stderr,"Illegal condition at address 0x%x\n",addr);
     		exit(1);
     	case ERR_IMMEDIATE:	
-    		printf("Immediate value forbidden at address 0x%x\n",addr); 
+    		fprintf(stderr,"Immediate value forbidden at address 0x%x\n",addr); 
     		exit(1);
     	case ERR_SEGTEXT:
-    		printf("Segmentation fault in text at address 0x%x\n",addr);
+    		fprintf(stderr,"Segmentation fault in text at address 0x%x\n",addr);
     		exit(1);	
     	case ERR_SEGDATA: 
-    		printf("Segmentation fault in data at address 0x%x\n",addr);	
+    		fprintf(stderr,"Segmentation fault in data at address 0x%x\n",addr);	
     		exit(1);
     	case ERR_SEGSTACK: 
-    		printf("Segmentation fault in stack at address 0x%x\n",addr);
+    		fprintf(stderr,"Segmentation fault in stack at address 0x%x\n",addr);
     		exit(1);
     	case ERR_NOERROR:
     		exit(0);
+        default:
+            exit(0);
 	}
 }
