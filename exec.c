246ebad16f7dbb01bd9e0342868e859b4656a254
Quentin <salmeron.quentin@etu.unice.fr>
2014-06-13 14:30:02 +0200

    Factorisasseuion

--- exec.c
+++ exec.c
@@ -3,7 +3,6 @@
 #include "error.h"
 #include "exec.h"
 
-
 //! Récupère l'adresse d'une instruction en mode absolu/indexé
 /*!
  * \param pmach la machine/programme en cours d'exécution
@@ -15,7 +14,6 @@ static unsigned address(Machine *pmach, Instruction instr)
 	if (instr.instr_generic._immediate) {
 		return instr.instr_immediate._value;
 	}
-
 	else if (instr.instr_generic._indexed) {
 		return pmach->_registers[instr.instr_indexed._rindex]
 			+ instr.instr_indexed._offset;
@@ -34,7 +32,6 @@ static bool jump_or_not(Machine *pmach, Instruction instr)
 	if (instr.instr_generic._regcond > LAST_CONDITION) {
 		error(ERR_CONDITION, pmach->_pc - 1);
 	}
-
 	switch (instr.instr_generic._regcond)
 	{
 		case NC:
@@ -56,28 +53,6 @@ static bool jump_or_not(Machine *pmach, Instruction instr)
 	}
 }
 
-//! Effectue un ILLOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return ne retourne rien car part en erreur
- */
-static bool illop(Machine *pmach, Instruction instr)
-{
-	error(ERR_ILLEGAL, pmach->_pc - 1);
-}
-
-//!	Effectue un NOP sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return vrai
- */
-static bool nop(Machine *pmach, Instruction instr)
-{
-	return true;
-}
-
 //!	Effectue un LOAD sur la machine
 /*!
  * \param pmach la machine/programme en cours d'exécution
@@ -87,7 +62,6 @@ static bool nop(Machine *pmach, Instruction instr)
 static bool load(Machine *pmach, Instruction instr)
 {
 	unsigned rc = instr.instr_generic._regcond;
-
 	if (instr.instr_generic._immediate) {
 		pmach->_registers[rc] = instr.instr_immediate._value;
 	} 
@@ -97,10 +71,8 @@ static bool load(Machine *pmach, Instruction instr)
 		if (addr >= pmach->_datasize) {
 			error(ERR_SEGDATA, pmach->_pc -1);
 		}
-
 		pmach->_registers[rc] = pmach->_data[addr];
 	}
-
 	if (pmach->_registers[rc] < 0) {
 		pmach->_cc = CC_N;
 	}
@@ -110,7 +82,6 @@ static bool load(Machine *pmach, Instruction instr)
 	else {
 		pmach->_cc = CC_P;
 	}
-
 	return true;
 }
 
@@ -125,41 +96,34 @@ static bool store(Machine *pmach, Instruction instr)
 	if (instr.instr_generic._immediate) {
 		error(ERR_IMMEDIATE, pmach->_pc - 1);
 	}
-
 	unsigned addr = address(pmach, instr);
-
 	if (addr >= pmach->_datasize) {
 		error(ERR_SEGDATA, pmach->_pc - 1);
 	}
-
 	pmach->_data[addr] = pmach->_registers[instr.instr_generic._regcond];
-
 	return true;
 }
 
-//!	Effectue un ADD sur la machine
+//!	Effectue un ADD ou un SUB sur la machine
 /*!
  * \param pmach la machine/programme en cours d'exécution
  * \param instr l'instruction à exécuter
  * \return vrai s'il n'y a pas d'erreur
  */
-static bool add(Machine *pmach, Instruction instr)
+static bool add_sub(Machine *pmach, Instruction instr)
 {
+	int sign = (instr.instr_generic._cop == ADD) ? 1 : -1;
 	unsigned rc = instr.instr_generic._regcond;
-
 	if (instr.instr_generic._immediate) {
-		pmach->_registers[rc] += instr.instr_immediate._value;
+		pmach->_registers[rc] += instr.instr_immediate._value * sign;
 	} 
 	else {
 		unsigned addr = address(pmach, instr);
-
 		if (addr >= pmach->_datasize) {
 			error(ERR_SEGDATA, pmach->_pc - 1);
 		}
-
-		pmach->_registers[rc] += pmach->_data[addr];
+		pmach->_registers[rc] += pmach->_data[addr] * sign;
 	}
-
 	if (pmach->_registers[rc] < 0) {
 		pmach->_cc = CC_N;
 	}
@@ -169,43 +133,6 @@ static bool add(Machine *pmach, Instruction instr)
 	else {
 		pmach->_cc = CC_P;
 	}
-
-	return true;
-}
-
-//!	Effectue un SUB sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return vrai s'il n'y a pas d'erreur
- */
-static bool sub(Machine *pmach, Instruction instr)
-{
-	unsigned rc = instr.instr_generic._regcond;
-
-	if (instr.instr_generic._immediate) {
-		pmach->_registers[rc] -= instr.instr_immediate._value;
-	} 
-	else {
-		unsigned addr = address(pmach, instr);
-
-		if (addr >= pmach->_datasize) {
-			error(ERR_SEGDATA, pmach->_pc - 1);
-		}
-
-		pmach->_registers[rc] -= pmach->_data[addr];
-	}
-
-	if (pmach->_registers[rc] < 0) {
-		pmach->_cc = CC_N;
-	}
-	else if (pmach->_registers[rc] == 0) {
-		pmach->_cc = CC_Z;
-	}
-	else {
-		pmach->_cc = CC_P;
-	}
-
 	return true;
 }
 
@@ -220,11 +147,9 @@ static bool branch(Machine *pmach, Instruction instr)
 	if (instr.instr_generic._immediate) {
 		error(ERR_IMMEDIATE, pmach->_pc - 1);
 	}
-
 	if (jump_or_not(pmach, instr)) {
 		pmach->_pc = address(pmach, instr);
 	}
-
 	return true;
 }
 
@@ -239,17 +164,14 @@ static bool call(Machine *pmach, Instruction instr)
 	if (instr.instr_generic._immediate) {
 		error(ERR_IMMEDIATE, pmach->_pc - 1);
 	}
-
 	if (jump_or_not(pmach, instr)) {
 		if (pmach->_sp < 0 || pmach->_sp >= pmach->_datasize) {
 			error(ERR_SEGSTACK, pmach->_pc - 1);
 		}
-
 		pmach->_data[pmach->_sp] = pmach->_pc;
 		pmach->_pc = address(pmach, instr);
 		pmach->_sp--;
 	}
-
 	return true;
 }
 
@@ -265,8 +187,6 @@ static bool ret(Machine *pmach, Instruction instr)
 	if (pmach->_sp < 0 || pmach->_sp >= pmach->_datasize) {
 		error(ERR_SEGSTACK, pmach->_pc - 1);
 	}
-
-
 	pmach->_pc = pmach->_data[pmach->_sp];
 	return true;
 }
@@ -282,7 +202,6 @@ static bool push(Machine *pmach, Instruction instr)
 	if (pmach->_sp < 0 || pmach->_sp >= pmach->_datasize) {
 		error(ERR_SEGSTACK, pmach->_pc - 1);
 	}
-
 	if (instr.instr_generic._immediate) {
 		pmach->_data[pmach->_sp] = instr.instr_immediate._value;
 	}
@@ -291,10 +210,8 @@ static bool push(Machine *pmach, Instruction instr)
 		if (addr >= pmach->_datasize) {
 			error(ERR_SEGDATA, pmach->_pc - 1);
 		}
-
 		pmach->_data[pmach->_sp] = pmach->_data[addr];
 	}
-
 	pmach->_sp--;
 	return true;
 }
@@ -314,47 +231,29 @@ static bool pop(Machine *pmach, Instruction instr)
 	if (pmach->_sp < 0 || pmach->_sp >= pmach->_datasize) {
 		error(ERR_SEGSTACK, pmach->_pc - 1);
 	}
-
 	unsigned addr = address(pmach, instr);
-
 	if (addr >= pmach->_datasize) {
 		error(ERR_SEGDATA, pmach->_pc - 1);
 	}
-
 	pmach->_data[addr] = pmach->_data[pmach->_sp];
-
 	return true;
 }
-
-//!	Effectue un HALT sur la machine
-/*!
- * \param pmach la machine/programme en cours d'exécution
- * \param instr l'instruction à exécuter
- * \return faux
- */
-static bool halt(Machine *pmach, Instruction instr)
-{
-	warning(WARN_HALT, pmach->_pc - 1);
-	return false;
-}
-
 
 bool decode_execute(Machine *pmach, Instruction instr)
 {
 	switch(instr.instr_generic._cop)
 	{
 		case ILLOP:
-			return illop(pmach, instr);
+			error(ERR_ILLEGAL, pmach->_pc - 1);
 		case NOP:
-			return nop(pmach, instr);
+			return true;
 		case LOAD:
 			return load(pmach, instr);
 		case STORE:
 			return store(pmach, instr);
 		case ADD:
-			return add(pmach, instr);
 		case SUB:
-			return sub(pmach, instr);
+			return add_sub(pmach, instr);
 		case BRANCH:
 			return branch(pmach, instr);
 		case CALL:
@@ -366,7 +265,8 @@ bool decode_execute(Machine *pmach, Instruction instr)
 		case POP:
 			return pop(pmach, instr);
 		case HALT:
-			return halt(pmach, instr);
+			warning(WARN_HALT, pmach->_pc - 1);
+			return false;
 		default:
 			error(ERR_UNKNOWN, pmach->_pc - 1);
 	}
